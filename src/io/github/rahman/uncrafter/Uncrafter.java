d04194cfa527eae3a873adedc9c7898ae550af57
Rahman93 <Rahman1993@hotmail.co.uk>
2014-06-30 14:03:27 +0100

    Shaped/Shapeless check
    Remove items also

--- src/io/github/rahman/uncrafter/Uncrafter.java
+++ src/io/github/rahman/uncrafter/Uncrafter.java
@@ -1,11 +1,14 @@
 package io.github.rahman.uncrafter;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.Recipe;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.ShapelessRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Uncrafter extends JavaPlugin {
@@ -13,8 +16,12 @@ public class Uncrafter extends JavaPlugin {
 	
 	public void onEnable(){
 		getLogger().info(loggerPrefix + "- Version " + this.getDescription().getVersion() + " has been enabled.");
-		//Add config pull
-		//getConfig().getList("Resources").get(x);
+		/* TODO
+		Add config pull
+		getConfig().getList("Resources").get(x);
+		Add enchantment removal
+		Allow for multiple items
+		*/
 	}
 
 	public boolean onCommand(final CommandSender sender, Command command, String label, String[] args) {
@@ -27,17 +34,29 @@ public class Uncrafter extends JavaPlugin {
             	   }
             	   
             	   int dura = ((Player) sender).getPlayer().getItemInHand().getDurability();
-            	   Recipe recipe = getServer().getRecipesFor(((Player) sender).getItemInHand()).get(0);
-            	   /*getServer().ShapelessRecipe.getIngredientList();
-            	     This is where I am having issues*/
             	   
             	   if(dura == 0){
-            		   sender.sendMessage(ChatColor.BLUE + "Uncrafted" + recipe);
+            		   
+            		   Recipe recipe = getServer().getRecipesFor(((Player) sender).getItemInHand()).get(0);
+            		   if (recipe instanceof ShapelessRecipe){
+            			   ((Player) sender).getInventory().setItemInHand(new ItemStack(Material.AIR)); 
+                		   sender.sendMessage(ChatColor.WHITE + "[" + ChatColor.BLUE + "Uncrafter" + ChatColor.WHITE + "] " + ChatColor.RED + "Shapeless");
+            		   }
+            		   
+            		   else if(recipe instanceof ShapedRecipe){
+            			   ((Player) sender).getInventory().setItemInHand(new ItemStack(Material.AIR)); 
+                		   sender.sendMessage(ChatColor.WHITE + "[" + ChatColor.BLUE + "Uncrafter" + ChatColor.WHITE + "] " + ChatColor.RED + "Shaped");
+            		   }
+            		   
+            		   else{
+            			   ((Player) sender).getInventory().setItemInHand(new ItemStack(Material.AIR)); 
+                		   sender.sendMessage(ChatColor.WHITE + "[" + ChatColor.BLUE + "Uncrafter" + ChatColor.WHITE + "] " + ChatColor.RED + "ERROR");
+            		   }
             		   return true;
             	   }
             	   
             	   if(dura != 0){
-            		   sender.sendMessage(ChatColor.BLUE + "Item is damaged! You must repair it before you try to uncraft!" + recipe);
+            		   sender.sendMessage(ChatColor.WHITE + "[" + ChatColor.BLUE + "Uncrafter" + ChatColor.WHITE + "] " + ChatColor.RED + "Item is damaged! You must repair it before you try to uncraft!");
             		   return true;
             	   }
             	   
