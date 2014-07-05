2ee70b5f2da886c81ca85d3e4f5d913aa2ba0ce1
Rahman93 <Rahman1993@hotmail.co.uk>
2014-07-05 07:23:28 +0100

    Add enchanted book
    not finished

--- src/io/github/rahman/uncrafter/Uncrafter.java
+++ src/io/github/rahman/uncrafter/Uncrafter.java
@@ -1,32 +1,36 @@
 package io.github.rahman.uncrafter;
 
 import java.util.ArrayList;
+//import java.util.List;
 import java.util.Map;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.Recipe;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.ShapelessRecipe;
+import org.bukkit.inventory.meta.EnchantmentStorageMeta;
 import org.bukkit.plugin.java.JavaPlugin;
-
+/* TODO
+Add config pull
+Add enchantment removal
+Proper error messages
+*/
 public class Uncrafter extends JavaPlugin {
 	public final String loggerPrefix = "[Uncrafter] ";
     public final String chatPrefix = ChatColor.WHITE + "[" + ChatColor.BLUE + "Uncrafter" + ChatColor.WHITE + "] ";
 	
+    //public List<String> blacklisted;
+    
 	public void onEnable(){
+		getConfig().options().copyDefaults(true);
+	    saveDefaultConfig();
 		getLogger().info(loggerPrefix + "- Version " + this.getDescription().getVersion() + " has been enabled.");
-		/* TODO
-		Add config pull
-		getConfig().getList("Resources").get(x);
-		Add enchantment removal
-		Allow for multiple items
-		Proper error messages
-		*/
 	}
 
 	public boolean onCommand(final CommandSender sender, Command command, String label, String[] args) {
@@ -37,20 +41,39 @@ public class Uncrafter extends JavaPlugin {
             	if(item==null){
                     return false;
             	}
+            	if(item.getAmount() > 1){
+            		//int multiplier = item.getAmount();
+            		sender.sendMessage(chatPrefix + ChatColor.RED + "You tried to uncraft too many items! The maximum is one at a time.");
+            		return true;
+            	}
             	Recipe recipe = getServer().getRecipesFor(player.getItemInHand()).get(0);
             	if(recipe == null) {
                     return false;
                 }
-            	if(player.getPlayer().getItemInHand().getDurability() == 0) {
+            	
+            	if(item.getDurability() == 0) {
                     if (recipe instanceof ShapelessRecipe) {
                         ArrayList<ItemStack> slist = (ArrayList<ItemStack>) ((ShapelessRecipe) recipe).getIngredientList();
+                        if (item.getEnchantments().size() >= 1) {
+                        	Map<Enchantment,Integer> stored = item.getEnchantments();
+                        	ItemStack resultStack = new ItemStack(Material.ENCHANTED_BOOK);
+                        	//add enchants
+                        	player.getInventory().addItem(resultStack);
+                        
                         player.getInventory().setItemInHand(new ItemStack(Material.AIR));
                         sender.sendMessage(chatPrefix + ChatColor.RED + "Your item has been uncrafted!");
                         for (ItemStack aStack : slist) {
                             player.getInventory().addItem(aStack);
                         }
+                        
                     } else if (recipe instanceof ShapedRecipe) {
-                        Map<Character, ItemStack> shapedmap = ((ShapedRecipe) recipe).getIngredientMap();
+                        Map<Character, ItemStack> shapedmap = ((ShapedRecipe) recipe).getIngredientMap();   
+                        if (item.getEnchantments().size() >= 1) {
+                        	Map<Enchantment,Integer> stored = item.getEnchantments();
+                        	ItemStack resultStack = new ItemStack(Material.ENCHANTED_BOOK);
+                        	//add enchants
+                        	player.getInventory().addItem(resultStack);
+                        }
                         player.getInventory().setItemInHand(new ItemStack(Material.AIR));
                         sender.sendMessage(chatPrefix + ChatColor.RED + "Your item has been uncrafted!");
                         for (ItemStack shapeditemstack : shapedmap.values()) {
@@ -58,11 +81,13 @@ public class Uncrafter extends JavaPlugin {
                                 player.getInventory().addItem(shapeditemstack);
                             }
                         }
+                        
                     } else {
                         player.getInventory().setItemInHand(new ItemStack(Material.AIR));
                         sender.sendMessage(chatPrefix + ChatColor.RED + "Your item has been uncrafted!");
                     }
                     return true;
+                    
                 } else {
                     sender.sendMessage(chatPrefix + ChatColor.RED + "Item is damaged! You must repair it before you try to uncraft!");
                     return true;
